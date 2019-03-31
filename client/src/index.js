9e08d5cb16c910ae9c5362bdc47320f084b6bdb8
kanghadong <hdkang2007@gmail.com>
2019-03-31 09:59:51 +0900

    heroku deployment2

--- client/src/index.js
+++ client/src/index.js
@@ -17,7 +17,7 @@ import { WebSocketLink } from 'apollo-link-ws';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:4000/graphql',
+  uri: process.env.NODE_ENV === 'production' ? 'wss://geopinshdkang2007.herokuapp.com/graphql' : 'ws://localhost:4000/graphql',
   options: {
     reconnect: true
   }
